0cf380d9e3e88c23c511e458f6861134d9e81ade
897051629 <31534325+897051629@users.noreply.github.com>
2018-09-01 23:33:12 +0800

    1
    1

--- src/DataToDB/PositionResultToMySQL.py
+++ src/DataToDB/PositionResultToMySQL.py
@@ -1,43 +1,42 @@
 import pymysql
 import traceback
 import os.path
-
-#清洗的数据上传至MySQL  学波
+#职位要求分析结果上传至MySQL  学波
 class DataMySQL:
     def __init__(self):
         self.__dataCount = 0  # 记录数
         self.__fileName = "" #录入文件名
     def SaveToMySQLDB(self,file,fileName):
-        self.__fileName = fileName[:-5]
+        self.__fileName = fileName
+        if self.__fileName == 'sum':
+            self.__fileName = file + '_' + self.__fileName
         print(self.__fileName + ": 开始录入")
         self.CreateTable() #创建表
         self.__dataCount = 0
         with open(file+'\\'+fileName,'r',encoding='utf-8') as fileDate:
             while True :
                 data = fileDate.readline()
-                if data == '' : print(self.__fileName + " 录入完成") ;return
-                self.__dataCount += 1
-                self.__list = data.split('<!sp>')
+                if self.__dataCount==0:
+                    self.__dataCount=1;continue
+                if data[:5] == 'total' : print(self.__fileName + " 录入完成") ;return
+                self.__list = data.split(' ')
                 self.SaveToDatabase()
+                self.__dataCount += 1
     def SaveToDatabase(self):
             db = pymysql.Connect(
                 host='47.100.11.75',
                 port=3306,
                 user='root',
                 passwd='5880940',
-                db='finalData',
+                db='analysisResults',
                 charset='utf8')
             cursor = db.cursor()
-            #list = ('1','2','3','4','5','6','7','8','9','10','11')
-            for x in range(0,11):
-                self.__list.append('')
-                self.__list[x] = self.__list[x].strip('\'')
-                self.__list[x] = self.__list[x].replace('\'','’')
-                #self.__list[x] = '\''+self.__list[x]+'\''
-
-            sql = "INSERT INTO `%s` VALUES ('%d','%s','%s', '%s', '%s', '%s', '%s', '%s','%s','%s','%s','%s')"%\
-                  (self.__fileName,self.__dataCount, self.__list[0], self.__list[1], self.__list[2], self.__list[3],self.__list[4],
-                   self.__list[5], self.__list[6], self.__list[7], self.__list[8], self.__list[9],self.__list[10])
+            lists=[]
+            for x in range(len(self.__list)):
+                if self.__list[x]!='':lists.append(self.__list[x])
+            sql = "INSERT INTO `%s` VALUES ('%d','%s','%d', '%f', '%f', '%f')"%\
+                  (self.__fileName,self.__dataCount, lists[0], int(lists[1]),
+                   float(lists[2]), float(lists[3]), float(lists[4]))
             try:
                 cursor.execute(sql)
                 db.commit()
@@ -51,23 +50,17 @@ class DataMySQL:
             port=3306,
             user='root',
             passwd='5880940',
-            db='finalData',
+            db='analysisResults',
             charset='utf8')
         cursor = db.cursor()
         sql = """
-                   CREATE TABLE `finalData`.`{}` (
+                   CREATE TABLE `analysisResults`.`{}` (
   `ID` INT NOT NULL,
-  `招聘单位` TEXT NULL,
-  `岗位名称` TEXT NULL,
-  `工资` CHAR(50) NULL,
-  `城市` CHAR(50) NULL,
-  `经验要求` CHAR(100) NULL,
-  `岗位性质` TEXT NULL,
-  `发布时间` CHAR(100) NULL,
-  `发布地点` TEXT NULL,
-  `公司福利` TEXT NULL,
-  `岗位要求` TEXT NULL,
-  `具体地点` TEXT NULL,
+  `名称` TEXT NULL,
+  `频数` INT NULL,
+  `频率` DOUBLE NULL,
+  `最高比率` DOUBLE NULL,
+  `相对比率` DOUBLE NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
@@ -81,7 +74,7 @@ DEFAULT CHARACTER SET = utf8;
         finally: db.close()
 #多文件录入（二级目录）
 def SaveMostFile():
-    workDir = "F:\TomySQL\最终数据"
+    workDir = "F:\TomySQL\职位要求分析结果"
     os.chdir(workDir)  # 路径
     for file in os.listdir(workDir):
         print('开始录入： '+file)
